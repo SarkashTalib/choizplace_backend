9cf1e19c255e6f5550a50d331d3364bd30fdab6e
Sarkash Talib <sarkash.talib@gmail.com>
2023-05-05 10:59:13 +0100

    Migration to v4.10

--- config/server.js
+++ config/server.js
@@ -1,7 +1,13 @@
+'use strict';
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    // Add this to not receive populated relations in webhooks
+    populateRelations: false,
   },
 });
